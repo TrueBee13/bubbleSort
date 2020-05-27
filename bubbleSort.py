547f5573b7363438862b57e3c1f94e721867a858
Kateryna Demchyshyn <katerynademchyshyn@Katerynas-MacBook-Pro.local>
2020-05-26 22:39:54 -0400

    fixing mistakes with the look and removing ecptra prints

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,25 +1,12 @@
 import random
 
-
-def printArray(arr):
-	print('sorted Array: ')
-	for i in range(len(arr)):
-		print(arr[i])
-
 def bubbleSort(arr): 
     n = len(arr) 
     for i in range(n): 
         for j in range(0, n-i-1): 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
-'''
-for i in range(0,10000):
- a.append(random.randint(1,1000000))
-  
-bubbleSort(a)
-printArray(a)
-print('\n'.join(map(str, a))) 
-'''
+
 with open("generateNumbers.txt", "w") as myFile:
 	for i in range(1,1000):
     		myFile.write(str(random.randint(0,1000))+'\n')
@@ -30,12 +17,15 @@ try:
 		res=[]
 		data=f.read()
 		arr=data.split('\n')
-		for i in range(len(arr)-1):
-			res.append(int(i))
-		#print(res)
+		for i in arr:
+			try: 		
+				res.append(int(i))
+			except ValueError:
+				pass
+		
 	bubbleSort(res)
-	printArray(res)
 except FileNotFoundError:
 	data = None
-print(data)
+print("Sorted Arrey: ")
+print(str(res))
   	
